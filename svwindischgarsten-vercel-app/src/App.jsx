eee8f616f8f0250fcc0e57b493f38d0ae6f01dec
SamsonSuperMax <mschmidleitner@hotmail.com>
2025-06-26 00:26:01 +0200

    Add files via upload

---
+++ svwindischgarsten-vercel-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>SV Windischgarsten App geladen!</h1>
+      <p>Vercel-Deployment erfolgreich</p>
+    </div>
+  );
+}
+
+export default App;
