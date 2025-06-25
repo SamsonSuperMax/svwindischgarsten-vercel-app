cdac5362ea5518edb439652074905b8532d13e8d
SamsonSuperMax <mschmidleitner@hotmail.com>
2025-06-26 01:04:47 +0200

    Add files via upload

---
+++ svwindischgarsten-vercel-ready/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <div>Hallo vom SV Windischgarsten!</div>
+  </React.StrictMode>
+)
